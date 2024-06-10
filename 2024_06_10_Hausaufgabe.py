4897b5461260fe20d9afc38826d0ffcc34b5ac66
raflder1 <159057074+raflder1@users.noreply.github.com>
2024-06-10 18:32:05 +0200

    Create 2024_06_10_Hausaufgabe.py

---
+++ 2024_06_10_Hausaufgabe.py
@@ -0,0 +1,5 @@
+# Auf den 17.06.2024 als Hausaufgabe:
+
+# Schreibe eine Funktion aufsteigend_sortiert(liste: list) -> bool, die eine Liste von Integern erhält und
+# zurückgibt, ob diese aufsteigend sortiert ist oder nicht.
+# Z.B.: aufsteigend_sortiert([1, 4, 6, 6, 9, 10]) == True, aufsteigend_sortiert([10, 13, 4, 9]) == False.
