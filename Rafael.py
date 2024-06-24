91b1a202ab435fd4e2b57c3adb0e1de952d52192
raflder1 <rafael.renoirte@icloud.com>
2024-06-24 16:40:41 +0200

    Update Rafael.py

--- Rafael.py
+++ Rafael.py
@@ -1,5 +1,23 @@
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
+# Auf den 17.06.2024 als Hausaufgabe:
+
+# Schreibe eine Funktion aufsteigend_sortiert(liste: list) -> bool, die eine Liste von Integern erhält und
+# zurückgibt, ob diese aufsteigend sortiert ist oder nicht.
+# Z.B.: aufsteigend_sortiert([1, 4, 6, 6, 9, 10]) == True, aufsteigend_sortiert([10, 13, 4, 9]) == False.
+
+
+
+def aufsteigend_sortiert(liste: list) -> bool:
+
+
+
+    
+        if aufsteigend_sortiert <= 11:
+            print("Ja")
+        else:
+            print("Nein")
+
+
+        
+
+        aufsteigend_sortiert([1, 4, 6, 6, 9, 11])
+        aufsteigend_sortiert([10, 13, 4, 9])
